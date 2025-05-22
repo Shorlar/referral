0001e9dd24b65f5e30d02b5cf20b2eeabac2fd80
Babatope Adeoye <baarbz10@gmail.com>
2025-05-22 14:03:04 +0100

    Added api response

---
+++ src/main/java/org/example/utils/ApiResponse.java
@@ -0,0 +1,11 @@
+package org.example.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse <T> {
+    private String message;
+    private T data;
+}
